61c28b140cba431f360a3bc7da168b3e219f2567
Vladislav Bragin <dalvelab@gmail.com>
2024-03-04 13:03:10 +0500

    feat(strapi): change response structure for slider and footer

--- server/src/api/footer/controllers/footer.js
+++ server/src/api/footer/controllers/footer.js
@@ -6,4 +6,12 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::footer.footer');
+module.exports = createCoreController('api::footer.footer', ({strapi}) => ({
+  async find() {
+    const data = await strapi.entityService.findMany('api::footer.footer', {
+      populate: ['locations', 'socials'],
+    });
+
+    return { data };
+  },
+}));
--- server/src/api/slider/controllers/slider.js
+++ server/src/api/slider/controllers/slider.js
@@ -6,4 +6,12 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::slider.slider');
+module.exports = createCoreController('api::slider.slider', ({strapi}) => ({
+  async find() {
+    const data = await strapi.entityService.findMany('api::slider.slider', {
+      populate: ['slides', 'slides.event', 'slides.event.image'],
+    });
+
+    return { data };
+  },
+}));
